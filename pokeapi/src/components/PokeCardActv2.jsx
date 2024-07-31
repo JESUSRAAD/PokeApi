957a86d32354584a6515ba126d7522b577559951
Jesus Raad <jesus.raad@students.gammatech.school>
2024-07-31 02:15:00 +0200

    listo

--- pokeapi/src/components/PokeCardActv2.jsx
+++ pokeapi/src/components/PokeCardActv2.jsx
@@ -3,12 +3,17 @@ import { PokeContext } from "../context/PokemonContext";
 import { Swords } from "lucide-react";
 
 const PokeCardActv2 = ({ list }) => {
-    console.log(list);
-  
-  const {  setPokeId,copyDetailsPokemons,setCopyDetailsPokemons,pokeDetails, setPokeDetails,setFreshArrFuntin,limit} = useContext(PokeContext);
-  
+  console.log(list);
 
-
+  const {
+    setPokeId,
+    copyDetailsPokemons,
+    setCopyDetailsPokemons,
+    pokeDetails,
+    setPokeDetails,
+    setFreshArrFuntin,
+    limit,
+  } = useContext(PokeContext);
 
   const getPokeDetails = async () => {
     try {
@@ -23,15 +28,11 @@ const PokeCardActv2 = ({ list }) => {
         })
       );
 
-if (limit===20) {
-  setPokeDetails(data);
-} else {
-  setPokeDetails((prevDetails)=> [...prevDetails, ...data]);
-}
-
-
-
-     
+      if (limit === 20) {
+        setPokeDetails(data);
+      } else {
+        setPokeDetails((prevDetails) => [...prevDetails, ...data]);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -43,9 +44,6 @@ if (limit===20) {
 
   // setFreshArrFuntin( getPokeDetails())
   console.log(pokeDetails);
-
-
-
 
   return (
     <div className="flex gap-3 flex-wrap">
@@ -62,7 +60,7 @@ if (limit===20) {
             return (
               <div className=" mb-2">
                 <p className="flex font-light text-gray-700">
-                <Swords /> {stat.stat.name}:{stat.base_stat}
+                  <Swords /> {stat.stat.name}:{stat.base_stat}
                 </p>
               </div>
             );
@@ -70,35 +68,34 @@ if (limit===20) {
           return stat;
         };
 
-
-
-
         const handlePokeInfo = () => {
-        return  setPokeId(pokemon.id);
+          return setPokeId(pokemon.id);
         };
         const handlePokeDelete = (id) => {
-        return  setPokeDetails((prevDetails)=> prevDetails.filter((detail)=>detail.id!==id));
+          setPokeDetails((prevDetails) => {
+            const indexDelete = prevDetails.findIndex(
+              (detail) => detail.id === id
+            );
+
+            return [
+              ...prevDetails.slice(0, indexDelete),
+              ...prevDetails.slice(indexDelete + 1),
+            ];
+          });
         };
 
-
-
-
-
-
-        let cssClass=""
-if (pokemon.id%2===0) {
-  cssClass="bg-blue-500 text-white p-4"
-} else {
-  cssClass= "bg-blue-500 text-white p-4 bg-opacity-45"
-}
-
-
+        let cssClass = "";
+        if (pokemon.id % 2 === 0) {
+          cssClass = "bg-blue-500 text-white p-4";
+        } else {
+          cssClass = "bg-blue-500 text-white p-4 bg-opacity-45";
+        }
 
         return (
           <div className="max-w-max mx-auto bg-white border-2 border-gray-300 shadow-md rounded-lg overflow-hidden">
             <div className={cssClass}>
               <h2 className="text-2xl font-bold text-center">
-                {pokemon.name.toUpperCase()}   #{pokemon.id}
+                {pokemon.name.toUpperCase()} #{pokemon.id}
               </h2>
               <div className="flex justify-center bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtZk44zmb2phoudWrzYbI_cOdyZwVgbS29-g&s)] bg-center  text-white p-2 text-center rounded-t-xl">
                 {imgsPokemon()}
@@ -109,15 +106,18 @@ if (pokemon.id%2===0) {
                   <div>{pokeCard().slice(3, 6)}</div>
                 </div>
                 <div className="flex w-full gap-2">
-                <button onClick={()=>handlePokeInfo()} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-[80%]">
-                  Info
-                </button>
-                <button 
-                onClick={()=>handlePokeDelete(pokemon.id)} 
-                className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded w-[20%]">
-                  X
-                </button>
-
+                  <button
+                    onClick={() => handlePokeInfo()}
+                    className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-[80%]"
+                  >
+                    Info
+                  </button>
+                  <button
+                    onClick={() => handlePokeDelete(pokemon.id)}
+                    className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded w-[20%]"
+                  >
+                    X
+                  </button>
                 </div>
               </div>
             </div>
